91e9c739f7560050238c3dd29cb1286790b26283
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-06-03 18:40:47 +0200

    Don't add versions like '0.0.0' to previous artifacts
    This could happen with sbt-dynver generated versions like '0.0.0+3-70919203-SNAPSHOT'.

--- src/test/scala/sbtcompatibility/VersionsTests.scala
+++ src/test/scala/sbtcompatibility/VersionsTests.scala
@@ -24,6 +24,7 @@ object VersionsTests extends TestSuite {
       * -  check("1.1-RC2", "1.0")
       * - checkEmpty("1.0-RC2")
       * - checkEmpty("1.0-RC2+43")
+      * - checkEmpty("0.0.0+3-70919203-SNAPSHOT")
     }
 
     "compatible" - {
