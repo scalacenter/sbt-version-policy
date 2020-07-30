c727a29ff0b1225993b8401ef23faa264caed070
Eugene Yokota <eed3si9n@gmail.com>
2020-07-29 23:58:28 -0400

    Add documentation for the keys

--- sbt-compatibility/src/main/scala/sbtcompatibility/SbtCompatibilityKeys.scala
+++ sbt-compatibility/src/main/scala/sbtcompatibility/SbtCompatibilityKeys.scala
@@ -7,17 +7,17 @@ import sbtcompatibilityrules.SbtCompatibilityRulesPlugin
 
 trait SbtCompatibilityKeys {
   final val compatibilityPreviousArtifacts      = taskKey[Seq[ModuleID]]("")
-  final val compatibilityReportDependencyIssues = taskKey[Unit]("")
+  final val compatibilityReportDependencyIssues = taskKey[Unit]("Check for removed or updated dependencies in an incompatible way.")
   final val compatibilityCheck                  = taskKey[Unit]("Runs both compatibilityReportDependencyIssues and mimaReportBinaryIssues")
-  final val forwardCompatibilityCheck           = taskKey[Unit]("")
+  final val forwardCompatibilityCheck           = taskKey[Unit]("Report forward binary compatible issues from Mima.")
 
-  final val compatibilityIgnored                 = taskKey[Seq[OrganizationArtifactName]]("")
-  final val compatibilityCheckDirection          = taskKey[Direction]("")
+  final val compatibilityIgnored                 = taskKey[Seq[OrganizationArtifactName]]("Exclude these dependencies from compatibilityReportDependencyIssues.")
+  final val compatibilityCheckDirection          = taskKey[Direction]("Direction to check the compatibibility. Default: Direction.backward.")
 
-  final val compatibilityFindDependencyIssues = taskKey[Seq[(ModuleID, DependencyCheckReport)]]("")
+  final val compatibilityFindDependencyIssues = taskKey[Seq[(ModuleID, DependencyCheckReport)]]("Compatibility issues in the library dependencies.")
 
-  final val compatibilityDefaultRules          = taskKey[Seq[ModuleID]]("")
-  final val compatibilityDefaultReconciliation = taskKey[Option[VersionCompatibility]]("")
+  final val compatibilityDefaultRules          = taskKey[Seq[ModuleID]]("Fallback rules used to evalute compatibility issues in the library dependency.")
+  final val compatibilityDefaultReconciliation = taskKey[Option[VersionCompatibility]]("Fallback reconciliation used to evalute compatibility issues in the library dependency.")
 
   final val compatibilityInternal: SbtCompatibilityInternalKeys =
     new SbtCompatibilityInternalKeys {}
